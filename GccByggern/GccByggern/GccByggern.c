4bbeb995d7fb816a0d27cbe57a5c7b8cdba99c86
unknown <simenrau@itk-d655.win.ntnu.no>
2017-09-04 10:42:57 +0200

    'update'

--- GccByggern/GccByggern/GccByggern.c
+++ GccByggern/GccByggern/GccByggern.c
@@ -7,11 +7,14 @@
 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdint.h>
 
 int main(void)
 {
     while(1)
     {
-        //TODO:: Please write your application code 
+		    
     }
-}
+} 
