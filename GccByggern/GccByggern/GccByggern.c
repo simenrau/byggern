d401ce7e13897b52786349cd1923b7842776098f
unknown <simenrau@itk-d655.win.ntnu.no>
2017-09-04 14:08:14 +0200

    update

--- GccByggern/GccByggern/GccByggern.c
+++ GccByggern/GccByggern/GccByggern.c
@@ -1,27 +1,17 @@
 /*
  * GccByggern.c
  *
- * Created: 04.09.2017 09:14:04
+ * Created: 04.09.2017 11:16:20
  *  Author: simenrau
  */ 
 
 
-#include <stdio.h>
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <stdint.h>
-
 
 int main(void)
 {
-		
-	DDRA = 0x00;
-	DDRB = 0xFF;
-	
-	while(1)
-	{
-		PORTB = 100;
-	}	
-	return 0;
-} 
-
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
