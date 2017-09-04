d3f7f9b11b141bfd5b40df00a9dd5d93436a9bab
unknown <simenrau@itk-d655.win.ntnu.no>
2017-09-04 09:16:56 +0200

    'AtmelStudios-programming'

---
+++ GccByggern/GccByggern/GccByggern.c
@@ -0,0 +1,17 @@
+/*
+ * GccByggern.c
+ *
+ * Created: 04.09.2017 09:14:04
+ *  Author: simenrau
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
